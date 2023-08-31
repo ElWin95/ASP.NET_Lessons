9e52ff3c986992de18cf6838eff7da02dac79b70
ElWin95 <elvinma@code.edu.az>
2023-08-31 07:46:08 +0400

    'lesson01'

---
+++ 01. Lesson/P416FirstWebApp/P416FirstWebApp/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P416FirstWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        //public IActionResult Index(string name, string surname, int age)
+        //{
+        //    return Content($"{name} {surname} {age}"); 
+        //}
+        //public IActionResult Index(int id)
+        //{
+        //    return Content($"{id}");
+        //}
+        //public IActionResult Index()
+        //{
+        //    return Json(new
+        //    {
+        //        name="Filankes",
+        //        surname="Loremov",
+        //        age=34
+        //    });
+        //}
+        public IActionResult Index()
+        {
+            return File("~/img/website.jpg", "image/jpg");
+        }
+    }
+}
---
+++ 01. Lesson/P416FirstWebApp/P416FirstWebApp/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.addSqlServer();
+//builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();
+
+
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!"); localhost/user/test
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern:"{controller}/{action}/{id}"
+//    );
+
+app.MapControllerRoute(
+    "default",
+    "{controller=test}/{action=index}/{id?}");
+app.UseStaticFiles();
+
+
+app.Run();
