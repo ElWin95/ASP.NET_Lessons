6a2cd61706a44b063215862f607742994f1a7c70
ElWin95 <elvinma@code.edu.az>
2023-09-19 07:13:48 +0400

    '17'

---
+++ 04. Lesson/FiorelloP416app/FiorelloP416app/RegisterServices.cs
@@ -0,0 +1,18 @@
+﻿using FiorelloP416.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FiorelloP416
+{
+    public static class RegisterServices
+    {
+        public static void Register(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
