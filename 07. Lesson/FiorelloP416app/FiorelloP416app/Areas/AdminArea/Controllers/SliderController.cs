28be0c2b4215d30de8a6161714bb407dbe948f7c
ElWin95 <elvinma@code.edu.az>
2023-10-08 00:56:45 +0400

    '30'

--- 07. Lesson/FiorelloP416app/FiorelloP416app/Areas/AdminArea/Controllers/SliderController.cs
+++ 07. Lesson/FiorelloP416app/FiorelloP416app/Areas/AdminArea/Controllers/SliderController.cs
@@ -1,5 +1,6 @@
 ﻿using FiorelloP416.DAL;
 using FiorelloP416.Entities;
+using FiorelloP416app.Extension;
 using FiorelloP416app.ModelViews.AdminSlider;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ namespace FiorelloP416app.Areas.AdminArea.Controllers
     public class SliderController : Controller
     {
         private readonly AppDbContext _appDbContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public SliderController(AppDbContext appDbContext)
+        public SliderController(AppDbContext appDbContext, IWebHostEnvironment webHostEnvironment)
         {
             _appDbContext = appDbContext;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -31,21 +34,37 @@ namespace FiorelloP416app.Areas.AdminArea.Controllers
                 ModelState.AddModelError("Photo", "Bosh qoyma");
                 return View();
             }
-            if(!createSliderVM.Photo.ContentType.Contains("image/"))
+            if(!createSliderVM.Photo.CheckImage())
             {
                 ModelState.AddModelError("Photo", "Only image");
                 return View();
             }
-            if (createSliderVM.Photo.Length>1000)
+            if (createSliderVM.Photo.CheckImageSize(1000))
             {
                 ModelState.AddModelError("Photo", "Olchu boyukdur");
                 return View();
             }
+
             Slider slider = new();
-            slider.ImageUrl = "";
+            slider.ImageUrl = createSliderVM.Photo.SaveImage("img",_webHostEnvironment);
             _appDbContext.Sliders.Add(slider);
             _appDbContext.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if(id == null) return NotFound();
+            var existSlider = _appDbContext.Sliders.FirstOrDefault(s => s.Id == id);
+            if (existSlider == null) return NotFound();
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "img", existSlider.ImageUrl);
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _appDbContext.Sliders.Remove(existSlider);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
