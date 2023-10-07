2b82ce68833c8fa9730ac28650210bbb6664716b
ElWin95 <elvinma@code.edu.az>
2023-10-07 15:38:48 +0400

    '26'

---
+++ 06. Lesson/FiorelloP416app/FiorelloP416app/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using FiorelloP416.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiorelloP416app.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Categories.ToList());
+        }
+        public IActionResult Detail(int? id)
+        {
+            if (id == null) return NotFound();
+            var existCategory = _appDbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (existCategory == null) return NotFound();
+            return View(existCategory);
+        }
+    }
+}
