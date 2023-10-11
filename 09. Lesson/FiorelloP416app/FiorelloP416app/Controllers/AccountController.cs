a82e01ce2688a77a55b41d4b48e87c4e948cfb9a
ElWin95 <elvinma@code.edu.az>
2023-10-11 21:48:56 +0400

    '36'

---
+++ 09. Lesson/FiorelloP416app/FiorelloP416app/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using FiorelloP416app.Entities;
+using FiorelloP416app.ModelViews.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiorelloP416app.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if(!ModelState.IsValid) return View();
+            AppUser appUser = new();
+            appUser.FullName = registerVM.FullName;
+            appUser.Email = registerVM.Email;
+            appUser.UserName = registerVM.UserName;
+
+            IdentityResult result = await _userManager.CreateAsync(appUser, registerVM.Password);
+            if(!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(registerVM);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
